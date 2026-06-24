4f390e8a677c01b738290578ae78b77a93f03ab2
wanpixxx <infoflot.12@gmail.com>
2026-06-24 11:07:44 +0300

    uppdate

--- io.py
+++ io.py
@@ -1,7 +1,29 @@
-for i in range(11):
-    print('i`il become not russian')
-else:
-    print('пизда')
-n = int(input)
-for i in range(n):
-    print('анус')
+# for i in range(11):
+#     print('i`il become not russian')
+# else:
+#     print('пизда')
+# n = int(input())
+# for i in range(n):
+#     print('анус')
+# # Heart Drawing Code
+import turtle
+
+screen = turtle.Screen()
+screen.title("Heart Drawing")
+
+pen = turtle.Turtle()
+pen.color("red")
+pen.fillcolor("pink")
+pen.speed(5)
+
+pen.begin_fill()
+pen.left(140)
+pen.forward(180)
+pen.circle(-90, 200)
+pen.left(120)
+pen.circle(-90, 200)
+pen.forward(180)
+pen.end_fill()
+
+pen.hideturtle()
+turtle.done()
