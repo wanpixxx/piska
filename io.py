b42124965c1e34c4d40a954523e34baf94ba9a0c
wanpixxx <infoflot.12@gmail.com>
2026-06-25 20:09:44 +0300

    4

--- io.py
+++ io.py
@@ -1,11 +1,4 @@
-# for i in range(11):
-#     print('i`il become not russian')
-# else:
-#     print('пизда')
-# n = int(input())
-# for i in range(n):
-#     print('анус')
-# # Heart Drawing Code
+
 import turtle
 
 screen = turtle.Screen()
